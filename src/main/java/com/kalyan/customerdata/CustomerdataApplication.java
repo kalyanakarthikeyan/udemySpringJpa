106e053fa681917694bd15aeabddac0cd6d26c9c
kalyan <kalyankarthi11@gmail.com>
2021-07-21 12:10:30 +0530

    initial commit

---
+++ src/main/java/com/kalyan/customerdata/CustomerdataApplication.java
@@ -0,0 +1,13 @@
+package com.kalyan.customerdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdataApplication.class, args);
+	}
+
+}
