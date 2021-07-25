ebe2227272159d55b5adb2287b321b3878282ed1
kalyan <kalyankarthi11@gmail.com>
2021-07-25 12:09:36 +0530

    finder method with page and sort

--- src/main/java/com/kalyan/customerdata/repositories/CustomerRepository.java
+++ src/main/java/com/kalyan/customerdata/repositories/CustomerRepository.java
@@ -1,14 +1,20 @@
 package com.kalyan.customerdata.repositories;
 
 import com.kalyan.customerdata.entities.Customer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 
     @Modifying
     @Query("update Customer set email=:email where id=:id")
     public void updateCustomerEmail(@Param("id") int id, @Param("email") String email);
+
+
+    public List<Customer> findByIdIn(List<Integer> customerIds, Pageable pageable);
 }
--- src/test/java/com/kalyan/customerdata/CustomerdataApplicationTests.java
+++ src/test/java/com/kalyan/customerdata/CustomerdataApplicationTests.java
@@ -5,9 +5,13 @@ import com.kalyan.customerdata.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -67,6 +71,21 @@ class CustomerdataApplicationTests {
 	@Rollback(value = false)
 	public void testUpdateEmail(){
 		customerRepository.updateCustomerEmail(2, "updatedemail@test.com");
+	}
+
+
+	@Test
+	public void testCustomerIdInWithPageable(){
+		Sort sort = Sort.by(Sort.Direction.DESC, "id");
+		Pageable pageable = PageRequest.of(0, 2, sort);
+		customerRepository.findByIdIn(Arrays.asList(1,2,3,4,5,6,7,8,9,10), pageable).forEach(System.out::println);
+	}
+
+	@Test
+	public void testCustomerIdInWithPageable1(){
+		Sort sort = Sort.by(Sort.Direction.DESC, "id");
+		Pageable pageable = PageRequest.of(0, 5, sort);
+		customerRepository.findByIdIn(Arrays.asList(1,2,3,4,5,6,7,8,9,10), pageable).forEach(System.out::println);
 	}
 
 }
